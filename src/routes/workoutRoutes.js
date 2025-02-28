ca88c2bdc13b50b0d26bd6ffde0f0a4924eecf83
ラナ <rana@sees.tokyo>
2025-02-28 09:53:50 +0900

    Updated workoutRoutes.js

--- src/routes/workoutRoutes.js
+++ src/routes/workoutRoutes.js
@@ -7,11 +7,10 @@ const router = Router();
 // Basic CRUD routes
 router.get("/", WorkoutController.getAll);
 router.get("/:id", WorkoutController.getOne);
-// router.post("/", WorkoutController.create);
-// router.put("/update/:id", WorkoutController.update);
-router.delete("/:id", WorkoutController.delete);
-
 router.post("/", WorkoutController.create);
 router.patch("/:id", WorkoutController.update);
+router.delete("/:id", WorkoutController.delete);
+
+
 
 export default router;
