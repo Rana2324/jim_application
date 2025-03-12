1c8f4ea157e563e9e224acc11c3693b4141f8ac4
ラナ <rana@sees.tokyo>
2025-03-12 16:49:54 +0900

    added tailwind css winston,morgan

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
-import logger from './logger';
+import logger from './logger.js';
 
 
 dotenv.config();
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,14 +1,4 @@
-// export const errorHandler = (err, req, res, next) => {
-//     console.error('Error:', err);
 
-//     const status = err.status || 500;
-//     const message = err.message || 'Internal Server Error';
-
-//     res.status(status).json({
-//         status: 'error',
-//         message
-//     });
-// };
 
 import chalk from "chalk";
 
