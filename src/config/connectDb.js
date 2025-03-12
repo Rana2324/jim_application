d9db1cd472d1fea9835bcabf130458d8e2943dcf
ラナ <rana@sees.tokyo>
2025-03-12 16:43:02 +0900

    added tailwind css winston,morgan

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,5 +1,7 @@
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
+import logger from './logger';
+
 
 dotenv.config();
 
@@ -10,10 +12,13 @@ const connectDb = async () => {
         }
 
         const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        // console.log(`MongoDB Connected: ${conn.connection.host}`);
+        logger.info(`MongoDB Connected: ${conn.connection.host}`);
         return conn;
     } catch (error) {
-        console.error(`MongoDB Connection Error: ${error.message}`);
+        // console.error(`MongoDB Connection Error: ${error.message}`);
+        logger.error(`MongoDB Connection Error: ${error.message}`);
+
         process.exit(1); // Exit if we can't connect to MongoDB
     }
 };
